589d0127816c0106530885e3ea026832025b6eb3
nmcollins <NMcollins-cc@conestogac.on.ca>
2016-11-18 13:14:03 -0500

    Initial Set Up
    Common Interview Programming Problem, find the buy and sell time of a
    stock to maximize profit for the day

---
+++ StockProblem/StockProblem.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace StockProblem
+{
+    partial class StockProblem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxStockPrices = new System.Windows.Forms.TextBox();
+            this.txtBxPurchasePrice = new System.Windows.Forms.TextBox();
+            this.txtBxPurchaseTime = new System.Windows.Forms.TextBox();
+            this.txtBxSellPrice = new System.Windows.Forms.TextBox();
+            this.txtBxSellTime = new System.Windows.Forms.TextBox();
+            this.lblPurchasePrice = new System.Windows.Forms.Label();
+            this.lblPurchaseTime = new System.Windows.Forms.Label();
+            this.lblSellPrice = new System.Windows.Forms.Label();
+            this.lblSellTime = new System.Windows.Forms.Label();
+            this.lblStockPrices = new System.Windows.Forms.Label();
+            this.txtBxProfit = new System.Windows.Forms.TextBox();
+            this.lblProfit = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBxStockPrices
+            // 
+            this.txtBxStockPrices.Location = new System.Drawing.Point(13, 82);
+            this.txtBxStockPrices.Multiline = true;
+            this.txtBxStockPrices.Name = "txtBxStockPrices";
+            this.txtBxStockPrices.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBxStockPrices.Size = new System.Drawing.Size(231, 109);
+            this.txtBxStockPrices.TabIndex = 0;
+            this.txtBxStockPrices.TabStop = false;
+            // 
+            // txtBxPurchasePrice
+            // 
+            this.txtBxPurchasePrice.Location = new System.Drawing.Point(13, 271);
+            this.txtBxPurchasePrice.Name = "txtBxPurchasePrice";
+            this.txtBxPurchasePrice.Size = new System.Drawing.Size(100, 20);
+            this.txtBxPurchasePrice.TabIndex = 0;
+            this.txtBxPurchasePrice.TabStop = false;
+            // 
+            // txtBxPurchaseTime
+            // 
+            this.txtBxPurchaseTime.Location = new System.Drawing.Point(120, 270);
+            this.txtBxPurchaseTime.Name = "txtBxPurchaseTime";
+            this.txtBxPurchaseTime.Size = new System.Drawing.Size(100, 20);
+            this.txtBxPurchaseTime.TabIndex = 0;
+            this.txtBxPurchaseTime.TabStop = false;
+            // 
+            // txtBxSellPrice
+            // 
+            this.txtBxSellPrice.Location = new System.Drawing.Point(226, 270);
+            this.txtBxSellPrice.Name = "txtBxSellPrice";
+            this.txtBxSellPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtBxSellPrice.TabIndex = 0;
+            this.txtBxSellPrice.TabStop = false;
+            // 
+            // txtBxSellTime
+            // 
+            this.txtBxSellTime.Location = new System.Drawing.Point(333, 270);
+            this.txtBxSellTime.Name = "txtBxSellTime";
+            this.txtBxSellTime.Size = new System.Drawing.Size(100, 20);
+            this.txtBxSellTime.TabIndex = 0;
+            this.txtBxSellTime.TabStop = false;
+            // 
+            // lblPurchasePrice
+            // 
+            this.lblPurchasePrice.AutoSize = true;
+            this.lblPurchasePrice.Location = new System.Drawing.Point(13, 252);
+            this.lblPurchasePrice.Name = "lblPurchasePrice";
+            this.lblPurchasePrice.Size = new System.Drawing.Size(79, 13);
+            this.lblPurchasePrice.TabIndex = 5;
+            this.lblPurchasePrice.Text = "Purchase Price";
+            // 
+            // lblPurchaseTime
+            // 
+            this.lblPurchaseTime.AutoSize = true;
+            this.lblPurchaseTime.Location = new System.Drawing.Point(120, 251);
+            this.lblPurchaseTime.Name = "lblPurchaseTime";
+            this.lblPurchaseTime.Size = new System.Drawing.Size(78, 13);
+            this.lblPurchaseTime.TabIndex = 6;
+            this.lblPurchaseTime.Text = "Purchase Time";
+            // 
+            // lblSellPrice
+            // 
+            this.lblSellPrice.AutoSize = true;
+            this.lblSellPrice.Location = new System.Drawing.Point(226, 250);
+            this.lblSellPrice.Name = "lblSellPrice";
+            this.lblSellPrice.Size = new System.Drawing.Size(51, 13);
+            this.lblSellPrice.TabIndex = 7;
+            this.lblSellPrice.Text = "Sell Price";
+            // 
+            // lblSellTime
+            // 
+            this.lblSellTime.AutoSize = true;
+            this.lblSellTime.Location = new System.Drawing.Point(333, 251);
+            this.lblSellTime.Name = "lblSellTime";
+            this.lblSellTime.Size = new System.Drawing.Size(50, 13);
+            this.lblSellTime.TabIndex = 8;
+            this.lblSellTime.Text = "Sell Time";
+            // 
+            // lblStockPrices
+            // 
+            this.lblStockPrices.AutoSize = true;
+            this.lblStockPrices.Location = new System.Drawing.Point(13, 63);
+            this.lblStockPrices.Name = "lblStockPrices";
+            this.lblStockPrices.Size = new System.Drawing.Size(67, 13);
+            this.lblStockPrices.TabIndex = 9;
+            this.lblStockPrices.Text = "Stock Prices";
+            // 
+            // txtBxProfit
+            // 
+            this.txtBxProfit.Location = new System.Drawing.Point(440, 270);
+            this.txtBxProfit.Name = "txtBxProfit";
+            this.txtBxProfit.Size = new System.Drawing.Size(100, 20);
+            this.txtBxProfit.TabIndex = 0;
+            this.txtBxProfit.TabStop = false;
+            // 
+            // lblProfit
+            // 
+            this.lblProfit.AutoSize = true;
+            this.lblProfit.Location = new System.Drawing.Point(440, 250);
+            this.lblProfit.Name = "lblProfit";
+            this.lblProfit.Size = new System.Drawing.Size(31, 13);
+            this.lblProfit.TabIndex = 11;
+            this.lblProfit.Text = "Profit";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(16, 13);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(300, 26);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "Given a list of stock prices and times from the previous day, \r\nfind the time to " +
+    "buy and sell that would give the maximum profit";
+            // 
+            // StockProblem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 444);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblProfit);
+            this.Controls.Add(this.txtBxProfit);
+            this.Controls.Add(this.lblStockPrices);
+            this.Controls.Add(this.lblSellTime);
+            this.Controls.Add(this.lblSellPrice);
+            this.Controls.Add(this.lblPurchaseTime);
+            this.Controls.Add(this.lblPurchasePrice);
+            this.Controls.Add(this.txtBxSellTime);
+            this.Controls.Add(this.txtBxSellPrice);
+            this.Controls.Add(this.txtBxPurchaseTime);
+            this.Controls.Add(this.txtBxPurchasePrice);
+            this.Controls.Add(this.txtBxStockPrices);
+            this.Name = "StockProblem";
+            this.Text = "Stock Problem";
+            this.Load += new System.EventHandler(this.StockProblem_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxStockPrices;
+        private System.Windows.Forms.TextBox txtBxPurchasePrice;
+        private System.Windows.Forms.TextBox txtBxPurchaseTime;
+        private System.Windows.Forms.TextBox txtBxSellPrice;
+        private System.Windows.Forms.TextBox txtBxSellTime;
+        private System.Windows.Forms.Label lblPurchasePrice;
+        private System.Windows.Forms.Label lblPurchaseTime;
+        private System.Windows.Forms.Label lblSellPrice;
+        private System.Windows.Forms.Label lblSellTime;
+        private System.Windows.Forms.Label lblStockPrices;
+        private System.Windows.Forms.TextBox txtBxProfit;
+        private System.Windows.Forms.Label lblProfit;
+        private System.Windows.Forms.Label lblDescription;
+    }
+}
+
---
+++ StockProblem/StockProblem.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockProblem
+{
+
+   
+
+    public partial class StockProblem : Form
+    {
+        public StockProblem()
+        {
+            InitializeComponent();
+        }
+
+        public class StockPriceTime
+        {
+            public int price;
+            public DateTime time;
+        }
+
+        private void get_max_profit(List<StockPriceTime> priceTimes)
+        {
+            int max=0;
+            StockPriceTime buyData = new StockPriceTime();
+            StockPriceTime sellData = new StockPriceTime();
+            for (int i=0; i<priceTimes.Count;i++)
+            {
+                for (int j=i+1;j<priceTimes.Count;j++)
+                {
+                    if (priceTimes[j].price - priceTimes[i].price > max)
+                    {
+                        buyData.time = priceTimes[i].time;
+                        buyData.price = priceTimes[i].price;
+                        sellData.time = priceTimes[j].time;
+                        sellData.price = priceTimes[j].price;
+                        max = priceTimes[j].price - priceTimes[i].price;
+                    }
+                }
+
+            }
+            txtBxProfit.Text = max.ToString("C");
+            txtBxPurchasePrice.Text = buyData.price.ToString("C");
+            txtBxPurchaseTime.Text = buyData.time.ToString();
+            txtBxSellPrice.Text = sellData.price.ToString("C");
+            txtBxSellTime.Text = sellData.time.ToString();
+
+           
+        }
+
+        private void StockProblem_Load(object sender, EventArgs e)
+        {
+
+            List<StockPriceTime> yesterday_time_prices = new List<StockPriceTime>();
+           
+            int[] stock_prices_yesterday = new int[] { 10, 7, 5, 8, 11, 9 };
+            DateTime today = DateTime.Today;
+            DateTime yesterday = today.AddDays(-1);
+            DateTime openingTime = new DateTime(yesterday.Year, yesterday.Month, yesterday.Day, 9, 30, 0);
+            for (int i=0; i<stock_prices_yesterday.Length;i++)
+            {
+                txtBxStockPrices.Text += openingTime.AddMinutes(i).ToString();
+                txtBxStockPrices.Text += "     ";
+                txtBxStockPrices.Text += stock_prices_yesterday[i].ToString("C");
+                txtBxStockPrices.Text += "\r\n";
+
+                StockPriceTime currentPriceTime = new StockPriceTime();
+                currentPriceTime.price = stock_prices_yesterday[i];
+                currentPriceTime.time = openingTime.AddMinutes(i);
+                yesterday_time_prices.Add(currentPriceTime);
+
+            }
+
+
+            get_max_profit(yesterday_time_prices);
+        }
+    }
+}
